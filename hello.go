5173ad8189c5f88c0853d4dc69d4319d8e2e8ec1
Caleb Curry <caleb@calebcurry.com>
2024-04-23 06:02:14 -0400

    creating our own interface

--- hello.go
+++ hello.go
@@ -51,12 +51,49 @@ func (points Points) String() string {
 	return str
 }
 
+type Magnitude interface {
+	Abs() float64
+}
+
+type PointXYZ struct {
+	X, Y, Z float64
+}
+
+func (p PointXYZ) Abs() float64 {
+	return math.Sqrt(p.X*p.X + p.Y*p.Y + p.Z*p.Z)
+}
+
+func (p PointXYZ) String() string {
+	return fmt.Sprintf("(%.2f, %.2f, %.2f) -> %f", p.X, p.Y, p.Z, p.Abs())
+}
+
+type Magnitudes []Magnitude
+
+func (m Magnitudes) Len() int {
+	return len(m)
+}
+
+func (m Magnitudes) Less(i, j int) bool {
+	return m[i].Abs() < m[j].Abs()
+}
+
+func (m Magnitudes) Swap(i, j int) {
+	m[i], m[j] = m[j], m[i]
+}
+
 func main() {
-	points := Points{
-		{3, 4},
-		{5, 10},
-		{1, 2},
+	points := Magnitudes{
+		Point{3, 4},
+		Point{5, 10},
+		Point{1, 2},
+		PointXYZ{1, 2, 3},
 	}
+
 	sort.Sort(points)
-	fmt.Println(points)
+
+	for _, point := range points {
+		fmt.Println(point)
+		fmt.Println(point.Abs())
+	}
+
 }
