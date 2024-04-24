80d011230cde785a1319569b9df3625237d01eb2
Caleb Curry <caleb@calebcurry.com>
2024-04-24 06:45:24 -0400

    Pointer examples

--- hello.go
+++ hello.go
@@ -2,12 +2,46 @@ package main
 
 import (
 	"fmt"
-	"sort"
 
 	"github.com/calebcurry/go-points"
 )
 
+func Swap(x, y *int) {
+	fmt.Println("Inside function, before swap", *x, *y)
+	*x, *y = *y, *x
+	fmt.Println("Inside function, after swap", *x, *y)
+}
+
+func ModifyPoint(point *points.Point) {
+	point.X = 50000
+	fmt.Println(point.X)
+}
+
+func ModifyArray(data *[3]int) {
+	data[1] = 50000
+	fmt.Println(*data)
+}
+
+func ModifySlice(data []int) {
+	data[1] = 50000
+	fmt.Println(data)
+}
+
+func Clear(pts *points.Magnitudes) {
+	*pts = points.Magnitudes{
+		points.Point{X: 30, Y: 4},
+		points.Point{X: 5, Y: 10},
+		points.Point{X: 10, Y: 2},
+		points.PointXYZ{X: 1, Y: 2, Z: 3},
+	}
+	fmt.Println("Inside function:", pts)
+}
+
 func main() {
+	data := []int{1, 2, 3}
+	ModifySlice(data)
+	fmt.Println(data)
+
 	pts := points.Magnitudes{
 		points.Point{X: 3, Y: 4},
 		points.Point{X: 5, Y: 10},
@@ -15,10 +49,6 @@ func main() {
 		points.PointXYZ{X: 1, Y: 2, Z: 3},
 	}
 
-	sort.Sort(pts)
-
-	for _, point := range pts {
-		fmt.Println(point)
-		fmt.Println(point.Abs())
-	}
+	Clear(&pts)
+	fmt.Println("After function:", pts)
 }
