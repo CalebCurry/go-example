64fc234c6a796248329eb94dea118f2fe7c1d6bc
Caleb Curry <caleb@calebcurry.com>
2024-04-24 08:27:19 -0400

    basic goroutine example

--- hello.go
+++ hello.go
@@ -2,48 +2,32 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/calebcurry/go-points"
+	"time"
 )
 
-func Swap(x, y *int) {
-	fmt.Println("Inside function, before swap", *x, *y)
-	*x, *y = *y, *x
-	fmt.Println("Inside function, after swap", *x, *y)
+func greet(name string, channel chan string) {
+	channel <- fmt.Sprintln("hello", name)
 }
 
-func ModifyPoint(point *points.Point) {
-	point.X = 50000
-	fmt.Println(point.X)
-}
-
-func ModifyArray(data *[3]int) {
-	data[1] = 50000
-	fmt.Println(*data)
-}
-
-func ModifySlice(data []int) {
-	data[1] = 50000
-	fmt.Println(data)
-}
-
-func Clear(pts *points.Magnitudes) {
-	*pts = nil
-	fmt.Println("Inside function:", pts)
+func greetByLetter(name string, channel chan string) {
+	time.Sleep(3 * time.Second)
+	channel <- fmt.Sprintln("hello", name)
 }
 
 func main() {
-	data := []int{1, 2, 3}
-	ModifySlice(data)
-	fmt.Println(data)
-
-	pts := points.Magnitudes{
-		points.Point{X: 3, Y: 4},
-		points.Point{X: 5, Y: 10},
-		points.Point{X: 1, Y: 2},
-		points.PointXYZ{X: 1, Y: 2, Z: 3},
+	channel := make(chan string)
+	go greet("John", channel)
+	go greetByLetter("Sal", channel)
+	for i := 0; i < 2; i++ {
+		select {
+		case response := <-channel:
+			fmt.Println("received:", response)
+		case <-time.After(4 * time.Second):
+			fmt.Println("Timeout occured")
+		}
 	}
-
-	Clear(&pts)
-	fmt.Println("After function:", pts)
+	// message := <-channel
+	// fmt.Println(message)
+	// message = <-channel
+	// fmt.Println(message)
 }
